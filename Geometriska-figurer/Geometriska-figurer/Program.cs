897ca18c14d6f0541f23d5e901bb790117bfa376
unknown <vr222bt@student.lnu.se>
2014-10-14 17:54:37 +0200

    Implemented ReadDoubleGreaterThanZero

--- Geometriska-figurer/Geometriska-figurer/Program.cs
+++ Geometriska-figurer/Geometriska-figurer/Program.cs
@@ -14,12 +14,39 @@ namespace Geometriska_figurer
             Rectangle rekt = new Rectangle(20, 5);
             Console.WriteLine(rekt.ToString());
 
+            rekt.Width = ReadDoubleGreaterThanZero("Ange längd:");
+            Console.WriteLine(rekt.ToString());
             
         }
 
         private static double ReadDoubleGreaterThanZero(string prompt)
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                try
+                {
+                    double number = double.Parse(input);
+                    if (number > 0)
+                    {
+                        return number;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("{0} måste vara större än 0!", number);
+                        Console.ResetColor();
+                    }
+
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEL! {0} är inte i önskat format, du måste ange ett tal större än noll.", input);
+                    Console.ResetColor();
+                } 
+            }
         }
 
         private static void ViewMenu()
